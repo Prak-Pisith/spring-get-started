a1765abd1ab8ff1365e499688bc572a07e81ef5b
The_Pi$ith <pisith@Pisiths-MBP.bbrouter>
2024-04-19 22:48:26 +0700

    Initial commit

---
+++ src/test/java/pisith/xyz/getstarted/GetStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package pisith.xyz.getstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
